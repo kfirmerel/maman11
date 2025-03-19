b7d2496181f35870e8cab25ec0e0b161673e3f0e
manoryuval <yuvalmanor98@gmail.com>
2025-03-19 23:10:53 +0200

    remove old code

--- ques2/count_zero_bits.c
+++ ques2/count_zero_bits.c
@@ -6,39 +6,9 @@
 */
 
 int palindrome(char s[ ]){
-	int left = 0;
-    int right = strlen(s) - 1;
-
-    while (left < right) {
-        // skip if tas/space
-        while (left < right && (s[left]=='\t' || s[left]==' '))
-			left++;
-        while (left < right && (s[right]=='\t' || s[right]==' '))
-			right--;
-        //comparing char
-        if (s[left]-s[right]) {
-            return 0; // not palindrome
-        }
-        left++;
-        right--;
-    }
-
-    return 1; // palindrome
+	
 }
 
 int main() {
-	char input[81]; //place to 80 char. for finish - '\0'
-
-    printf("Enter a string (max 80 chars): ");
-    fgets(input, sizeof(input), stdin);
-
-    printf("You entered: %s\n", input);
-	int len = strlen(input);
-    if (len > 0 && input[len - 1] == '\n')
-        input[len - 1] = '\0';
-	if (palindrome(input))
-		printf("the text is a palindrome\n");
-	else
-		printf("the text is NOT a palindrome\n");
-    return 0;
+return 0;
 }
