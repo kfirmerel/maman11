96e1a38aade23931499b8f3631510661379248e8
kfirmerel <kfird773@gmail.com>
2025-03-20 11:36:57 +0200

    ssdSX

--- palindrome/palindrome.c
+++ palindrome/palindrome.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 #include <string.h>
-#define isok(c) (((c) >= 'a' && (c) <= 'z') || ((c) >= 'A' && (c) <= 'Z') || ((c) >= '0' && (c) <= '9') || ((c) == ' ') || ((c) == '\t')) /* macro to make sure the char is a letter or a number */
 int palindrome(char s[]){ /* function to check if the string is a palindrome */
     int left = 0;  
     int right = strlen(s) - 1;
     while (left<right){
-        if (!isok(s[left]) || !isok(s[right])){ /*check if the char is a letter or a number*/ 
-            printf("The string is not include only letters and numbers\n");
-            return 0;
-        } 
         if (s[left] == ' ' || s[left] == '\t'){ /*skip if tab/space */
             left++;
             continue;
@@ -26,16 +21,16 @@ int palindrome(char s[]){ /* function to check if the string is a palindrome */
 return 1;
 }
 int main(){
-    char s[81];
-    int len;
+    char input[81]; /*max 80 char + /n */
+    int length; /*length of the string*/
     printf("Enter a string:\n");
-    fgets(s, sizeof(s), stdin);
-    printf("the string you enterd is : %s\n", s);
-    len = strlen(s);
-    if (s[len - 1] == '\n'){
-        s[len - 1] = '\0';
+    fgets(input, sizeof(input), stdin);
+    printf("the string you enterd is : %s\n", input);
+    length = strlen(input);
+    if (input[length - 1] == '\n'){
+        input[length - 1] = '\0'; /*removing the /n from the string*/
     }
-    if (palindrome(s)){
+    if (palindrome(input)){
         printf("The string is a palindrome\n");
     }
     else{
