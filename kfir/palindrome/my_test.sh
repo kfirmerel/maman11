98dd72cd3a37d86779bd4a26d1fb2b13297d8271
kfirmerel <kfird773@gmail.com>
2025-03-30 20:39:42 +0300

    finely

--- kfir/palindrome/my_test.sh
+++ kfir/palindrome/my_test.sh
@@ -1,4 +1,12 @@
 make
-for ((i=1; i<=10; i++)); do
-    ./palindrome < "test$i"
+for ((i=1; i<=5; i++)); do
+    ./palindrome < "test$i" > "temp$i.out"
+done
+for ((i=1; i<=5; i++)); do
+    diff "temp$i.out" "temp$i.exp"  
+    if [ $? -ne 0 ]; then
+        echo "Test $i failed"
+    else
+        echo "Test $i passed"
+    fi
 done
